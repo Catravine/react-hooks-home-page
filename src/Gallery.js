54563634dcc66938448cda2466acd142ef318ca1
Caroline Courtney <caroline@carolinecourtney.com>
2019-09-02 13:10:01 -0400

    Can adjust the delay and increment of the gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,25 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import PICTURES from './data/pictures';
 
+const SECONDS = 1000;
+const minimumDelay = 1 * SECONDS;
+const minimumIncrement = 1;
+
 function Gallery() {
   const [index, setIndex] = useState(0);
+  const [delay, setDelay] = useState(3 * SECONDS);
+  const [increment, setIncrement] = useState(1);
 
   useEffect(() => {
+    console.log('delay', delay, 'increment', increment);
     const interval = setInterval(() => {
       setIndex(
         storedIndex => {
-          return (storedIndex + 1) % PICTURES.length
+          return (storedIndex + increment) % PICTURES.length
         });
-    }, 3000);
+    }, delay);
 
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [delay, increment]);
+
+  const updateDelay = event => {
+    const delay = Number(event.target.value) * SECONDS;
+    setDelay(delay < minimumDelay ? minimumDelay : delay);
+  }
+
+  const updateIncrement = event => {
+    const increment = Number(event.target.value)
+    setIncrement(increment < minimumIncrement ? minimumIncrement : increment);
+  }
 
   return (
     <div className='Gallery'>
       <img src={PICTURES[index].image} alt='gallery' />
+      <div className='mulitform'>
+        <div>
+          Gallery transsision delay (seconds):
+          <input type='number' onChange={updateDelay} />
+        </div>
+        <div>
+          Gallery increment:
+          <input type='number' onChange={updateIncrement} />
+        </div>
+      </div>
     </div>
   )
 }
